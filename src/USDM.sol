06d74dc43665d4945f7755486a5a7ece52ad9b20
Stan Takt <st.takt@gmail.com>
2025-06-27 12:51:52 +0300

    added getAllStableRate for Unisweap V2, V3 WIP

--- src/USDM.sol
+++ src/USDM.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract USDM is ERC20, ERC20Burnable {
-    constructor() ERC20("MultiUSD", "USDM") {
+contract USDM is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("MultiUSD", "USDM") Ownable(msg.sender) {
+        // Initial minting to the owner
+        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1 million tokens
     }
-    function mint (address to, uint256 amount) public {
+    function mint (address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
+    //todo add to transfer() some fee to owner ))
 }
