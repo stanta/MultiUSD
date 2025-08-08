aa0c623858b9f3a7585f0ca50e3f96273b0fd972
Stan Takt <st.takt@gmail.com>
2025-08-08 07:44:40 +0300

    renamed balancer to Corrector,  added rate correct logic WIP

--- src/USDM.sol
+++ src/USDM.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.24;
 
-import "@openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract USDM is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("MultiUSD", "USDM") Ownable(msg.sender) {
